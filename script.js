4756c3ef1a6205a1612153a1c43d8c683779ed83
SergeyDobrev <dobrev.sergey@gmail.com>
2018-01-27 11:22:06 +0200

    3th commit

--- script.js
+++ script.js
@@ -1,66 +1,50 @@
 var numArray = [
-			   [[0, 1, 1, 1, 0],		//  9
-				[1, 0, 0, 0, 1],
-				[1, 0, 0, 0, 1],
-				[0, 1, 1, 1, 1],
-				[0, 0, 0, 0, 1],	
-				[0, 0, 0, 0, 1],
-				[1, 0, 0, 0, 1],	
-				[0, 1, 1, 1, 0]],
-
-			   [[0, 1, 1, 1, 0],		//	8
-				[1, 0, 0, 0, 1],
-				[1, 0, 0, 0, 1],
-				[1, 0, 0, 0, 1],
-				[1, 0, 0, 0, 1],	
-				[1, 0, 0, 0, 1],
-				[1, 0, 0, 0, 1],	
-				[0, 1, 1, 1, 0]]
-
-			   [[0, 0, 1, 0, 0],		//	7
-				[0, 1, 1, 0, 0],
-				[0, 0, 1, 0, 0],
-				[0, 0, 1, 0, 0],
-				[0, 0, 1, 0, 0],	
-				[0, 0, 1, 0, 0],
-				[0, 0, 1, 0, 0],	
-				[0, 1, 1, 1, 0]],
-
-			   [[0, 0, 1, 0, 0],		//	6
-				[0, 1, 1, 0, 0],
-				[0, 0, 1, 0, 0],
-				[0, 0, 1, 0, 0],
-				[0, 0, 1, 0, 0],	
-				[0, 0, 1, 0, 0],
-				[0, 0, 1, 0, 0],	
-				[0, 1, 1, 1, 0]],
-
-			   [[0, 0, 1, 0, 0],		//	5
-				[0, 1, 1, 0, 0],
-				[0, 0, 1, 0, 0],
-				[0, 0, 1, 0, 0],
-				[0, 0, 1, 0, 0],	
-				[0, 0, 1, 0, 0],
-				[0, 0, 1, 0, 0],	
-				[0, 1, 1, 1, 0]]
-
+			   [[' ','X','X','X',' '],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],[' ','X','X','X',' ']],
+			   [[' ',' ','X',' ',' '],[' ','X','X',' ',' '],[' ',' ','X',' ',' '],[' ',' ','X',' ',' '],[' ',' ','X',' ',' '],[' ',' ','X',' ',' '],[' ',' ','X',' ',' '],[' ','X','X','X',' ']],
+			   [[' ','X','X','X',' '],['X',' ',' ',' ','X'],[' ',' ',' ',' ','X'],[' ',' ',' ','X',' '],[' ',' ','X',' ',' '],[' ','X',' ',' ',' '],['X',' ',' ',' ',' '],['X','X','X','X','X']],
+			   [[' ','X','X','X',' '],['X',' ',' ',' ','X'],[' ',' ',' ',' ','X'],[' ',' ',' ','X',' '],[' ',' ',' ',' ','X'],[' ',' ',' ',' ','X'],['X',' ',' ',' ','X'],[' ','X','X','X',' ']],
+			   [[' ',' ',' ','X',' '],[' ',' ','X','X',' '],[' ','X',' ','X',' '],['X',' ',' ','X',' '],['X','X','X','X','X'],[' ',' ',' ','X',' '],[' ',' ',' ','X',' '],[' ',' ','X','X','X']],
+			   [['X','X','X','X',' '],['X',' ',' ',' ',' '],['X','X','X','X',' '],['X',' ',' ',' ','X'],[' ',' ',' ',' ','X'],[' ',' ',' ',' ','X'],['X',' ',' ',' ','X'],[' ','X','X','X',' ']],
+			   [[' ','X','X','X',' '],['X',' ',' ',' ','X'],['X',' ',' ',' ',' '],['X','X','X','X',' '],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],[' ','X','X','X',' ']],
+			   [['X','X','X','X','X'],[' ',' ',' ',' ','X'],[' ',' ',' ','X',' '],[' ',' ','X',' ',' '],[' ','X',' ',' ',' '],['X',' ',' ',' ',' '],['X',' ',' ',' ',' '],['X',' ',' ',' ',' ']],
+			   [[' ','X','X','X',' '],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],[' ','X','X','X',' '],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],[' ','X','X','X',' ']],
+			   [[' ','X','X','X',' '],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],['X',' ',' ',' ','X'],[' ','X','X','X','X'],[' ',' ',' ',' ','X'],['X',' ',' ',' ','X'],[' ','X','X','X',' ']],
 				];
+
+var wh = $(window).height();
+$('body').css({	'margin': '0 auto',
+				'background-color': 'black',
+				'overflow': 'hidden'
+			})
 
 var obj = document.createElement('div');
 document.body.appendChild(obj);
-$(obj).addClass('bigblock');
+$(obj).css({	'height': wh * .4 + 'px',
+				'width': wh * .4 + 'px',
+				'margin': wh * .2 + 'px auto',
+				'text-align': 'center'
+			})
+
 
 var obj1 = document.createElement('div');
 $(obj).append(obj1);
-$(obj1).addClass('rotateblock');
+$(obj1).css({	'height': wh * .4 + 'px',
+				'width': wh * .4 + 'px'
+			})
+$(obj1).addClass('preserve3d');
 
-var myArray = ['front', 'back', 'right', 'left', 'top', 'bottom'];
+var myArray = ['', 'rotateY(180deg)', 'rotateY(-90deg)', 'rotateY(90deg)', 'rotateX(90deg)', 'rotateX(-90deg)'];
 
 for (var i = 0; i < myArray.length; i++) {
-	var classNameVar = 'side ' + myArray[i];
 	var obj2 = document.createElement('div');
 	$(obj1).append(obj2);
-	$(obj2).addClass(classNameVar);
+	$(obj2).css({	'height': wh * .4 + 'px',
+					'width': wh * .4 + 'px',
+					'margin': '0 auto',
+					'position': 'absolute',
+					'border': '1px solid rgba(0,255,0,0.2)',
+					'transform': myArray[i] + 'translateZ(' + wh * .2 + 'px)'
+				})
 }
 
 var obj3 = new Array();
@@ -71,47 +55,79 @@ for (var x = 0; x < 8; x++) {
 	for (var y = 0; y < 5; y++) {
 		obj3[x][y] = document.createElement('div');
 		$(obj1).append(obj3[x][y]);
-		$(obj3[x][y]).addClass('smallblock');
-		$(obj3[x][y]).css('top', (10 + x * 23) + 'px');
-		$(obj3[x][y]).css('left', (46 + y * 23) + 'px');
-		var z = 100 - Math.floor(Math.random()*200);
+
+		$(obj3[x][y]).addClass('preserve3d');
+		$(obj3[x][y]).css({ 'height': wh * .04 + 'px',
+							'width': wh * .04 + 'px',
+							'display': 'inline-block',
+							'position': 'absolute',
+							'top': (wh * .02 + x * wh * .046) + 'px',
+							'left': (wh * .092 + y * wh * .046) + 'px'
+							});
+
+		var z = wh * .3 - Math.floor(Math.random()*(wh * .6));
 		obj3[x][y].style.transform = 'translateZ(' + z + 'px)';
 
 		for (var i = 0; i < myArray.length; i++) {
 			var obj4 = document.createElement('div');
 			$(obj3[x][y]).append(obj4);
-			var classNameVar = 'smallside small' + myArray[i];
-			$(obj4).addClass(classNameVar);
+			$(obj4).css({ 	'height': wh * .04 + 'px',
+							'width': wh * .04 + 'px',
+							'margin': '0 auto',
+							'position': 'absolute',
+							'border': '1px solid rgba(0,255,0,0.1)',
+							'transform': myArray[i] + 'translateZ(' + wh * .02 + '10px)',
+						});
+
 			if (i === 0) {
-				$(obj4).css({'backgroundColor' : 'rgba(0,255,0,0.5)', 'color':'#0f0'});	
+				$(obj4).css('backgroundColor', 'rgba(0,255,0,0.5)');	
 				obj5[x][y] = obj4;		
 			}
 		}
 	}
 }
 
-var i = 0;
-var intervalID = setInterval(myfun, 3500);
-function myfun() {
+var i = -1;
+var intervalID = setInterval(myIntervalFun, 2500);
+function myIntervalFun() {
 	i += 1;
-	obj1.style.transform = 
-		'rotateX(' + ((i % 2) * 180 + 0.5) + 'deg)' + 
-		'rotateY(' + ((i % 2) * 180 + 0.5) + 'deg)';
-	obj1.style.transition = 'transform 1.5s';
+
+	if (i % 2 === 0) {
+		obj1.style.transform = 'rotateX(45deg) rotateY(45deg)';
+	} else {
+		obj1.style.transform = 'rotateX(-45deg) rotateY(45deg)';		
+	}
+	obj1.style.transition = 'transform 0.5s';
 
 	for (var x = 0; x < obj3.length; x++) {
 		for (var y = 0; y < obj3[x].length; y++) {
-			var z = 100 - Math.floor(Math.random()*200);
-			obj3[x][y].style.transform =  
-				'rotateX(' + ((i % 2) * 360 + 0.5) + 'deg)' + 
-				'rotateY(' + ((i % 2) * 360 + 0.5) + 'deg)' +
-				'translateZ(' + z + 'px)';
-			obj3[x][y].style.transition = 'transform 2s';
-			if (numArray[(i % 5)][x][y] === 0 ) {
-				$(obj5[x][y]).css('backgroundColor', 'rgba(0,0,0,0)');
-			} else if (numArray[(i % 5)][x][y] === 1 ) {
-				$(obj5[x][y]).css('backgroundColor', 'rgba(0,255,0,0.4)');
+
+			if (numArray[(i % 10)][x][y] === ' ' ) {
+				$(obj5[x][y]).fadeOut(1500);
+			
+			} else if (numArray[(i % 10)][x][y] === 'X' ) {
+				$(obj5[x][y]).fadeIn(1500);					
 			}		
+	
 		}
+	}
+
+	var TimerId = setTimeout(myTimeoutFun, 500);
+	var TimerId2 = setTimeout(myTimeoutFun2, 1000);
+
+	function myTimeoutFun() {
+		for (var x = 0; x < obj3.length; x++) {
+			for (var y = 0; y < obj3[x].length; y++) {
+
+				var z = wh * .3 - Math.floor(Math.random()*(wh * .6));
+				obj3[x][y].style.transform = 'rotateX(0deg) rotateY(0deg) translateZ(' + z + 'px)';
+				obj3[x][y].style.transition = 'transform 1s';
+			}
+		}
+	}
+
+	function myTimeoutFun2() {
+		obj1.style.transform = 'rotateX(0deg) rotateY(0deg)';
+		obj1.style.transition = 'transform 0.5s';		
 	}
 }
